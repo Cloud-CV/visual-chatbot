65ceb1d2ec4ad21b0c85d87974c0c30ef12db021
Deshraj Yadav <deshraj@gatech.edu>
2018-09-25 15:43:23 -0400

    Update download_models.sh

--- models/download_models.sh
+++ models/download_models.sh
@@ -7,9 +7,9 @@ cd ..
 cd models
 wget https://gist.githubusercontent.com/ksimonyan/211839e770f7b538e2d8/raw/ded9363bd93ec0c770134f4e387d8aaaaa2407ce/VGG_ILSVRC_16_layers_deploy.prototxt
 wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
-wget https://computing.ece.vt.edu/~abhshkdz/visdial-deploy/hre-qih-g-new.t7
+wget https://s3.amazonaws.com/visual-dialog/models/v0.9/hre-ques-im-hist-gen-vgg16-14.t7
 cd ..
 
 mkdir data && cd data
-wget https://computing.ece.vt.edu/~abhshkdz/visdial-deploy/chat_processed_params_0.9.json
+wget https://s3.amazonaws.com/cvmlp/visdial-pytorch/data/chat_processed_params_0.9.json
 cd ..
