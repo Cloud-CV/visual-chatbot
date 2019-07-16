a8980587386de6ea9e8425a2a35be124f6ba280f
Yash Kant <ysh.kant@gmail.com>
2019-07-16 17:55:13 -0400

    Fix downloads file and requirements.txt

--- viscap/download_models.sh
+++ viscap/download_models.sh
@@ -8,7 +8,7 @@ mkdir captioning/model_data
 wget -O captioning/model_data/vocabulary_captioning_thresh5.txt https://dl.fbaipublicfiles.com/pythia/data/vocabulary_captioning_thresh5.txt
 wget -O captioning/model_data/detectron_model.pth  https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.pth
 wget -O captioning/model_data/butd.pth https://dl.fbaipublicfiles.com/pythia/pretrained_models/coco_captions/butd.pth
-wget -O captioning/model_data/butd.yaml https://dl.fbaipublicfiles.com/pythia/pretrained_models/coco_captions/butd.yml
+wget -O captioning/model_data/butd.yml https://dl.fbaipublicfiles.com/pythia/pretrained_models/coco_captions/butd.yml
 wget -O captioning/model_data/detectron_model.yaml https://dl.fbaipublicfiles.com/pythia/detectron_model/detectron_model.yaml
 wget -O captioning/model_data/detectron_weights.tar.gz https://dl.fbaipublicfiles.com/pythia/data/detectron_weights.tar.gz
 tar xf captioning/model_data/detectron_weights.tar.gz -C captioning/
